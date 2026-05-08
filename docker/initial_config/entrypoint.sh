cb3d670d1f6ddf64c9e66ee3e61b4ec4887119f5
Dalton <dalton@pukiru.games>
2026-05-08 10:38:37 -0400

    Add bootstrap container to preconfigure Forgejo and its admin user.

---
+++ docker/initial_config/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+FORGEJO_USER="git"
+FORGEJO_DIR="/data/gitea"
+FORGEJO_CONFIG_DIR="${FORGEJO_DIR}/conf"
+mkdir -p "$FORGEJO_CONFIG_DIR"
+FORGEJO_CONFIG_FILE_PATH="${FORGEJO_CONFIG_DIR}/app.ini"
+if [ ! -f "$FORGEJO_CONFIG_FILE_PATH" ]; then
+  cp /initial_config/app.ini "$FORGEJO_CONFIG_FILE_PATH"
+fi
+chown -R "$FORGEJO_USER" "$FORGEJO_DIR"
+echo "Attempting to migrate databases."
+su -c "forgejo migrate" $FORGEJO_USER
+su -c "forgejo admin user create --admin --email ${ADMIN_EMAIL} --username ${ADMIN_USER} --password ${ADMIN_PASSWORD}" $FORGEJO_USER
