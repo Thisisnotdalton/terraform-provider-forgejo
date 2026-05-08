201b3bdd14b03c3f52d21a1d6e4dc42e89f991c5
Dalton <Thisisnotdalton@users.noreply.github.com>
2026-05-08 13:54:40 -0400

    Bootstrap docker forgejo (#1)
    * Add bootstrap container to preconfigure Forgejo and its admin user.
    
    * Add health checks for Forgejo and database services.
    
    * Generate env file with FORGEJO_API_TOKEN for admin user. Ignore temporary admin access token.
    
    * Added flake.nix to automatically recreate Forgejo with nix shell hook and export env variable for shell use. 
    
    * Update test actions to use simplified docker compose command.
    
    * Use mirror image.
    
    ---------
    
    Co-authored-by: Dalton <dalton@pukiru.games>

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [
+                pkgs.gnumake
+                pkgs.go
+                pkgs.opentofu
+            ];
+            shellHook = ''
+                cd docker && docker compose down -v --remove-orphans && docker compose up -d
+                source access_tokens/env.admin_token
+                export FORGEJO_API_TOKEN
+                cd ../examples
+            '';
+          };
+        };
+      }
+    );
+}
